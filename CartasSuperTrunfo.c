d934fb37157aa41d307491a6e7c4c1c4b3b9312c
billybomba <105425417+billybomba@users.noreply.github.com>
2025-05-09 22:04:32 -0300

    Update CartasSuperTrunfo.c

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -4,7 +4,7 @@ int main()
 {
     // Declaracao de variaveis
     int populacao[2], pontos[2];
-    float area[2], pib[2];
+    float area[2], pib[2], densidadep[2], pibper[2];
     char estado[2][10], CDcarta[2][10], Ncidade[2][10];
 
     // Cadastramento carta 1
@@ -50,15 +50,23 @@ int main()
 
     printf("Digite o Numero de Pontos Turisticos \n");
     scanf(" %i", &pontos[1]);
+   
+    //Calculo PIB per Capita
+    pibper[0] = (float) pib[0]/populacao[0];
+    pibper[1] = (float) pib[1]/populacao[1];
+   
+    //Calculo Densidade Populacional
+    densidadep[0] = (float) populacao[0]/area[0];
+    densidadep[1] = (float) populacao[1]/area[1];
 
     // Imprimir na tela Carta 1
-    printf(" Carta : 1\n Estado : %s\n Codigo : %s\n Nome da Cidade : %s\n Populacao : %i\n Area: %.2f\n PIB: %.2f\n Numero de Pontos Turisticos: %i\n ", &estado[0], &CDcarta[0], &Ncidade[0], populacao[0], area[0], pib[0], pontos[0]);
+    printf(" Carta : 1\n Estado : %s\n Codigo : %s\n Nome da Cidade : %s\n Populacao : %i\n Area: %.2f\n PIB: %.2f\n Numero de Pontos Turisticos: %i\n Densidade Populacional: %.2f\n PIB per Capita: %.2f\n ", &estado[0], &CDcarta[0], &Ncidade[0], populacao[0], area[0], pib[0], pontos[0], densidadep[0], pibper[0]);
 
     // Pula linha
     printf("\n");
 
     // Imprimi na tela Carta 1
-    printf(" Carta : 2\n Estado : %s\n Codigo : %s\n Nome da Cidade : %s\n Populacao : %i\n Area: %.2f\n PIB: %.2f\n Numero de Pontos Turisticos: %i\n ", &estado[1], &CDcarta[1], &Ncidade[1], populacao[1], area[1], pib[1], pontos[1]);
+    printf(" Carta : 2\n Estado : %s\n Codigo : %s\n Nome da Cidade : %s\n Populacao : %i\n Area: %.2f\n PIB: %.2f\n Numero de Pontos Turisticos: %i\n Densidade Populacional: %.2f\n PIB per Capita: %.2f\n ", &estado[1], &CDcarta[1], &Ncidade[1], populacao[1], area[1], pib[1], pontos[1], densidadep[1], pibper[1]);
 
     return 0;
 }
